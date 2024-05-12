d1bb9c236c218088bba72b41c1ed3892f49434e5
Jacob Merson <mersoj2@rpi.edu>
2024-05-12 04:10:51 -0400

    use delegating constructor for FEAStep

--- src/mumfim/analysis/amsiFEA.cc
+++ src/mumfim/analysis/amsiFEA.cc
@@ -12,7 +12,8 @@
 #include "amsiBoundaryConditions.h"
 #include "apfFunctions.h"
 
-namespace amsi {
+namespace amsi
+{
   void FEAStep::RenumberDOFs()
   {
     if (!numbered)
@@ -101,7 +102,8 @@ namespace amsi {
                    std::vector<DirichletBCEntry> dbc,
                    std::vector<NeumannBCEntry> nbc,
                    const std::string & analysis_name,
-                   MPI_Comm cm, bool own_mesh)
+                   MPI_Comm cm,
+                   bool own_mesh)
       : constraint_dofs(0)
       , local_dof_count(0)
       , first_local_dof(0)
@@ -124,81 +126,78 @@ namespace amsi {
     analysis_dim = apf_mesh->getDimension();
   }
 
+  static ModelDefinition GetModelDefinitionComponent(
+      const mt::CategoryNode & analysis_case,
+      const std::string & analysis_name, const std::string& component_name)
+  {
+    auto* component = mt::GetCategoryByType(&analysis_case, component_name);
+    auto* temp = mt::GetCategoryByType(component, analysis_name);
+    component = (temp == nullptr) ? component : temp;
+    if(component == nullptr) {
+      throw mumfim::mumfim_error(component_name + " not defined");
+    }
+    return {
+        .associated = mt::AssociateModel(component),
+        .unassociated = std::make_shared<mt::CategoryNode>(*component)};
+  }
+
   FEAStep::FEAStep(apf::Mesh * mesh,
                    const mt::CategoryNode & analysis_case,
                    std::vector<DirichletBCEntry> dbc,
                    std::vector<NeumannBCEntry> nbc,
                    const std::string & analysis_name,
-                   MPI_Comm cm, bool own_mesh)
-      : constraint_dofs(0)
-      , local_dof_count(0)
-      , first_local_dof(0)
-      , global_dof_count(0)
-      , fixed_dofs(0)
-      , numbered(false)
-      , T(0.0)
-      , analysis_comm(cm)
-      , dirichlet_bcs(std::move(dbc))
-      , neumann_bcs(std::move(nbc))
-      , apf_mesh(mesh)
-      , apf_primary_field(nullptr)
-      , apf_primary_delta_field(nullptr)
-      , apf_primary_numbering(nullptr)
-      , own_mesh(own_mesh)
+                   MPI_Comm cm,
+                   bool own_mesh)
+      : FEAStep(
+            mesh,
+            GetModelDefinitionComponent(analysis_case,
+                                        analysis_name,
+                                        "problem definition"),
+            GetModelDefinitionComponent(analysis_case,
+                                        analysis_name,
+                                        "solution strategy"),
+            GetModelDefinitionComponent(analysis_case, analysis_name, "output"),
+            std::move(dbc),
+            std::move(nbc),
+            analysis_name,
+            cm,
+            own_mesh)
   {
-    const mt::CategoryNode* pd;
-    const mt::CategoryNode* ss;
-    const mt::CategoryNode* out;
-    const mt::CategoryNode* tmp;
-    pd = analysis_case.FindCategoryNodeByType("problem definition");
-    out = analysis_case.FindCategoryNodeByType("output");
-    ss = analysis_case.FindCategoryNodeByType("solution strategy");
-    if (pd == nullptr || ss == nullptr || out == nullptr) {
-      std::cerr << "analysis case should have \"problem "
-                   "definition\",\"output\", and \"solution strategy\".\n";
-      MPI_Abort(AMSI_COMM_WORLD, 1);
-    }
-    tmp = pd->FindCategoryNodeByType(analysis_name);
-    pd = (tmp == nullptr) ? pd : tmp;
-    tmp = ss->FindCategoryNodeByType(analysis_name);
-    ss = (tmp == nullptr) ? ss : tmp;
-    tmp = out->FindCategoryNodeByType(analysis_name);
-    out = (tmp == nullptr) ? out : tmp;
-    problem_definition = ModelDefinition{
-        .associated = mt::AssociateModel(pd),
-        .unassociated = std::make_shared<mt::CategoryNode>(*pd)};
-    solution_strategy = ModelDefinition{
-        .associated = mt::AssociateModel(ss),
-        .unassociated = std::make_shared<mt::CategoryNode>(*ss)};
-    output = ModelDefinition{
-        .associated = mt::AssociateModel(out),
-        .unassociated = std::make_shared<mt::CategoryNode>(*out)};
-    assert(solution_strategy.associated->GetNullGeometry() != nullptr);
-    analysis_dim = apf_mesh->getDimension();
   }
-  FEAStep::~FEAStep() {
-    if(own_mesh) {
+
+  FEAStep::~FEAStep()
+  {
+    if (own_mesh)
+    {
       apf::destroyMesh(apf_mesh);
     }
   }
+
   void FEAStep::setSimulationTime(double t)
   {
     T = t;
     if (!PCU_Comm_Self())
       std::cout << "Simulation time updated: " << T << std::endl;
   }
-  void FEAStep::GetDOFInfo(int& global, int& local, int& offset)
+
+  void FEAStep::GetDOFInfo(int & global, int & local, int & offset)
   {
     global = global_dof_count;
     local = local_dof_count;
     offset = first_local_dof;
   }
-  void assembleMatrix(LAS* las, int rw_cnt, int* rw_nms, int cl_cnt,
-                      int* cl_nms, double* Ke)
+
+  void assembleMatrix(LAS * las,
+                      int rw_cnt,
+                      int * rw_nms,
+                      int cl_cnt,
+                      int * cl_nms,
+                      double * Ke)
   {
     las->AddToMatrix(rw_cnt, rw_nms, cl_cnt, cl_nms, &Ke[0]);
   }
-  void assembleVector(LAS* las, int rw_cnt, int* rw_nms, double* fe)
+
+  void assembleVector(LAS * las, int rw_cnt, int * rw_nms, double * fe)
   {
     las->AddToVector(rw_cnt, rw_nms, &fe[0]);
   }
