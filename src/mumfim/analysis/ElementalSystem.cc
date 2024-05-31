cc9e10203d92a043a863e20c4510e61a13d031f3
Max Bloomfield <bloomm2@rpi.edu>
2024-05-31 16:40:24 -0400

    Trial: Loading field values at nodes in inElement

--- src/mumfim/analysis/ElementalSystem.cc
+++ src/mumfim/analysis/ElementalSystem.cc
@@ -1,21 +1,26 @@
 #include "ElementalSystem.h"
 #include <apfNumbering.h>
 #include <apfField.h>
+#include <apfElement.h>
 
 namespace amsi
 {
   // this function works for both scalar and vector fields
   static void GetNodalFieldValuesAndNumbers(apf::Field* field,
                                             apf::Numbering* numbering,
-                                            apf::MeshEntity* mesh_entity, std::vector<int>& dof_numbers, std::vector<double> & values) {
-    auto nnodes = field->countNodesOn(mesh_entity);
+                                            apf::Element *e, int nenodes, apf::MeshEntity *mesh_entity,
+                                            std::vector<int>& dof_numbers, std::vector<double> & values) {
+
     const auto ncomponents = apf::countComponents(field);
-    values.resize(ncomponents*nnodes);
-    dof_numbers.resize(ncomponents*nnodes);
-    for(int node=0; node<nnodes; ++node) {
-      apf::getComponents(field, mesh_entity, node, &values[node*ncomponents]);
-      for(int component=0; component<ncomponents; ++component) {
-        dof_numbers[node*ncomponents+component] = apf::getNumber(numbering,mesh_entity,node, component);
+    values.resize(ncomponents*nenodes);
+    dof_numbers.resize(ncomponents*nenodes);
+    apf::NewArray<double> new_array_data(ncomponents*nenodes);
+    e->getElementNodeData(new_array_data);
+    for(int node=0; node<nenodes; ++node) {
+      for(int component = 0; component < ncomponents; component++){
+        int index = node*ncomponents + component;
+        values[index] = new_array_data[index];
+        dof_numbers[index] = apf::getNumber(numbering,mesh_entity,node, component);
       }
     }
   }
@@ -35,7 +40,7 @@ namespace amsi
   }
 
   // if the new mesh elemenent is on a mesh entity with the same type as the
-  // previous mesh element was, we don't need to reallocate the memory for the
+  // previous mesh element was, we don't need to reallocate the memory for tht
   // element matrix and vector (just zero it), otherwise we need to resize it
   void ElementalSystem::inElement(apf::MeshElement * ME)
   {
@@ -45,7 +50,8 @@ namespace amsi
     int new_nedofs = nenodes * num_field_components;
     bool reallocate = nedofs != new_nedofs;
     nedofs = new_nedofs;
-    GetNodalFieldValuesAndNumbers(f, numbering_, getMeshEntity(ME),
+    apf::MeshEntity * mesh_entity = getMeshEntity(ME);
+    GetNodalFieldValuesAndNumbers(f, numbering_, e, nenodes, mesh_entity,
                                   field_numbers_, field_values_);
         // TODO: remove this check. login in DynamicArray will account for this
     // Note: the reallocation routine in DynamicArray will cause a lot of reallocations
