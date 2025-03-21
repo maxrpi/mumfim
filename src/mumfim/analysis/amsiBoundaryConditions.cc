4e3104f5198b2c427d7faf866929405aa722dbe8
Max Bloomfield <bloomm2@rpi.edu>
2025-03-20 22:39:14 -0400

    Don't add in a zero elemental mass matrix for NBC.

--- src/mumfim/analysis/amsiBoundaryConditions.cc
+++ src/mumfim/analysis/amsiBoundaryConditions.cc
@@ -446,9 +446,11 @@ namespace amsi {
             apf::getElementNumbers(nm, e, dofs);
             las->AddToVector(integrator->getnedofs(), &dofs[0],
                              &integrator->getFe()[0]);
-            las->AddToMatrix(integrator->getnedofs(), &dofs[0],
-                             integrator->getnedofs(), &dofs[0],
-                             &(integrator->getKe()(0,0)) );
+            if(integrator_type == amsi::NeumannBCType::robin){
+              las->AddToMatrix(integrator->getnedofs(), &dofs[0],
+                              integrator->getnedofs(), &dofs[0],
+                              &(integrator->getKe()(0,0)) );
+            }
             apf::destroyMeshElement(mnt);
           }
         }
